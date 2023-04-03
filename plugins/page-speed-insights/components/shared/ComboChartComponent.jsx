83183e5161adc0320167b73c029a984ab9d69182
IiIymik <sambotem@gmail.com>
2023-04-03 16:34:35 +0300

    add fix for chart

--- plugins/page-speed-insights/components/shared/ComboChartComponent.jsx
+++ plugins/page-speed-insights/components/shared/ComboChartComponent.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import {Chart} from 'react-google-charts'
 import {CATEGORIES} from '../../helpers/constants'
-
+// const testHistory = [
+//   [new Date('4/3/2023, 11:52 AM'), 56.00000000000001, 88, 92, 83, 70, 'April'],
+//   [new Date(2023, 3, 13), 55.00000000000001, 88, 92, 83, 70, 'April'],
+//   [new Date(2022, 3, 13), 57.99999999999999, 88, 92, 83, 70, 'April'],
+//   [new Date(2021, 3, 13), 56.00000000000001, 88, 92, 83, 70, 'May'],
+//   [new Date(2023, 3, 13), 55.00000000000001, 88, 92, 83, 70, 'May'],
+//   [new Date(2023, 3, 13), 57.99999999999999, 88, 92, 83, 70, 'May'],
+//   [new Date(2023, 3, 13), 56.00000000000001, 88, 92, 83, 70, 'March'],
+//   [new Date(2023, 3, 13), 55.00000000000001, 88, 92, 83, 70, 'March'],
+//   [new Date(2023, 3, 13), 57.99999999999999, 88, 92, 83, 70, 'March'],
+// ]
 const ComboChartComponent = ({history}) => {
   const options = {
     title: 'History Requests',
@@ -25,12 +35,21 @@ const ComboChartComponent = ({history}) => {
   }
 
   const columns = [
-    {type: 'string', id: 'Date'},
+    {type: 'date', id: 'Date'},
     ...CATEGORIES.map((item) => ({type: 'number', id: item})),
     {type: 'number', id: 'month'},
   ]
 
-  const data = [columns, ...history]
+  const data = [
+    columns,
+    ...history.map((item) => {
+      item[0] = new Date(item[0])
+      return item
+    }),
+    // ...testHistory,
+  ]
+
+  const showControls = new Set(history.map((item) => item[6])).size
 
   return (
     <>
@@ -39,28 +58,41 @@ const ComboChartComponent = ({history}) => {
         data={data}
         options={options}
         chartPackages={['corechart', 'controls']}
-        controls={[
-          {
-            controlEvents: [
-              {
-                eventName: 'statechange',
-                callback: ({chartWrapper, controlWrapper}) => {
-                  console.log('State changed to', controlWrapper?.getState())
+        controls={
+          showControls > 1
+            ? [
+                // {
+                //   controlEvents: [
+                //     {
+                //       eventName: 'statechange',
+                //       callback: ({chartWrapper, controlWrapper}) => {
+                //         // console.log('State changed to', controlWrapper?.getState())
+                //       },
+                //     },
+                //   ],
+                //   controlType: 'CategoryFilter',
+                //   options: {
+                //     filterColumnIndex: 6,
+                //     ui: {
+                //       labelStacking: 'vertical',
+                //       // label: 'Filter by date',
+                //       allowTyping: false,
+                //       allowMultiple: true,
+                //       caption: 'Choose a month',
+                //     },
+                //   },
+                // },
+                {
+                  controlType: 'DateRangeFilter',
+                  options: {
+                    // filterColumnLabel: 'Date',
+                    filterColumnIndex: 0,
+                    ui: {labelStacking: 'vertical'},
+                  },
                 },
-              },
-            ],
-            controlType: 'CategoryFilter',
-            options: {
-              filterColumnIndex: 6,
-              ui: {
-                labelStacking: 'vertical',
-                // label: 'Filter by date',
-                allowTyping: false,
-                allowMultiple: true,
-              },
-            },
-          },
-        ]}
+              ]
+            : undefined
+        }
       />
     </>
   )
