5c1270b0575a90b55a632d9d58a675cb463f55eb
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-05-07 16:43:13 +0300

    deleted globalStyles.ts

--- plugins/page-speed-monitoring/src/containers/PageSpeedInsightsContainer.tsx
+++ plugins/page-speed-monitoring/src/containers/PageSpeedInsightsContainer.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components'
 import {ThemeProvider as SanityThemeProvider, studioTheme} from '@sanity/ui'
 import PageSpeedInsightsGui from '../components/PageSpeedInsightsGui'
-import GlobalStyle from '../styles/globalStyles'
 import {Helmet} from 'react-helmet'
 
-export const Container = styled.div`
+export const MainContainer = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
@@ -31,16 +30,15 @@ export const Container = styled.div`
 const PageSpeedInsightsContainer = (props: any) => {
   return (
     <SanityThemeProvider theme={studioTheme}>
-      <GlobalStyle />
       <Helmet>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins&display=swap"
           rel="stylesheet"
         />
       </Helmet>
-      <Container>
+      <MainContainer>
         <PageSpeedInsightsGui {...props} />
-      </Container>
+      </MainContainer>
     </SanityThemeProvider>
   )
 }
