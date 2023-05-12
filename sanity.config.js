6dc0c345c0940fffa134b429b87c9be49f5436d8
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2023-05-12 14:49:07 +0300

    fixed plugin builder config

--- sanity.config.js
+++ sanity.config.js
@@ -7,7 +7,8 @@ import {SINGLETON_TYPES_LIST} from './helpers/constants'
 import Logo from './components/Logo'
 import {dashboardTool, projectInfoWidget, projectUsersWidget} from '@sanity/dashboard'
 // import {lighthousePlugin} from 'sanity-plugin-page-speed-monitoring-test'
-import {lighthousePlugin} from './plugins/page-speed-monitoring/src'
+// import {lighthousePlugin} from './plugins/page-speed-monitoring/src'
+import {lighthousePlugin} from 'sanity-lighthouse-plugin'
 
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID
 const dataset = process.env.SANITY_STUDIO_DATASET
