65c83e20fb6cb5df2e8b52c860097864fbfb8c84
Olezhka-web <oleh.protsyshyn.web@gmail.com>
2023-09-14 23:02:35 +0300

    chore: bump sanity-lighthouse-plugin version docs: updated docs for the plugin refactor: removed extra project files/packages

--- plugins/page-speed-insights/asset/CircleIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CircleIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
-      <rect width="17" height="17" rx="8.5" fill="#5CC971" />
-    </svg>
-  )
-}
--- plugins/page-speed-insights/index.js
+++
@@ -1,1 +0,0 @@
-export { pageSpeedPlugin } from './plugin';
